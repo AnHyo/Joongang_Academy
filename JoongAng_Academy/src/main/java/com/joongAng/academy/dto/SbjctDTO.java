ba1706e3814d0774ccbe90e4e81102f7039a89bd
애지 <minye0105@naver.com>
2023-04-19 09:43:57 +0900

    4/19 예지 commit

--- JoongAng_Academy/src/main/java/com/joongAng/academy/dto/SbjctDTO.java
+++ JoongAng_Academy/src/main/java/com/joongAng/academy/dto/SbjctDTO.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class SbjctDTO {
 
-	private String SBJCT_NO, SBJCT_NM, SBJCT_EXPLN, USE_YN, DEL_YN;
-	private int EDU_HR, rowcnt;
+	private String rowcnt,SBJCT_NO, SBJCT_NM, SBJCT_EXPLN, USE_YN, DEL_YN;
+	private int EDU_HR;
 }
