1458da5c0313734ded22f4363b211ce1be285baf
AnHyo <zlzlgy@naver.com>
2023-05-02 16:01:03 +0900

    5/2 효정: 학생 설문조사 페이지 추가

---
+++ JoongAng_Academy/src/main/java/com/joongAng/academy/dao/StuSurveyDAO.java
@@ -0,0 +1,26 @@
+package com.joongAng.academy.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.joongAng.academy.dto.StuSurveyDTO;
+
+@Repository
+@Mapper
+public interface StuSurveyDAO {
+
+	List<Map<String, Object>> surveySbj(String student_ID);
+
+	List<StuSurveyDTO> surveyTI(StuSurveyDTO stuSuvDTO);
+
+	List<StuSurveyDTO> surveyIn(StuSurveyDTO stuSuvTI);
+
+	List<StuSurveyDTO> surveyIn2(StuSurveyDTO stuSuvTI);
+
+	List<StuSurveyDTO> code();
+
+
+}
---
+++ JoongAng_Academy/src/main/java/com/joongAng/academy/dto/StuSurveyDTO.java
@@ -0,0 +1,17 @@
+package com.joongAng.academy.dto;
+
+import lombok.Data;
+
+@Data
+public class StuSurveyDTO {
+	private String student_ID, sbjctNo, cYear, cCd, cHalf;
+	private String DGSTFN_TITLE , DGSTFN_INTRO;
+	
+	/* APPL_DGSTFN_ITEM */
+	private String SBJCT_NO, DGSTFN_CN, QITEM_CD, DGSTFN_ANS_N, DGSTFN_NM, CRCLM_YEAR, CRCLM_CD, CRCLM_HALF;
+	private int DGSTFN_NO, SORT_SN;
+	
+	/* CODE_INFO */
+	private String CD, CD_NM;
+	
+}
